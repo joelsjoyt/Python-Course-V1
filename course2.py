51625c3ea7f36f97bc143e23ae439baa6dda8ce7
Joel S Joy <52008820+joelsjoyt@users.noreply.github.com>
2021-02-08 20:10:00 +0530

    Added files

---
+++ course2.py
@@ -0,0 +1,29 @@
+#no = input("Enter the number: ")
+#if (no % 2) == 0:
+#   print(str(no) + " Is an even number")
+a = 1
+b = 2
+if b < a:
+    print("True")
+elif b == 2:
+    print("Yeah")
+else:
+    print("false")
+# i = 0
+# while i <= 5:
+#     print(i)
+#     i = i + 1
+
+#for( i = 0; i < 5; i++)
+# for i in range(1,11):
+#     if (i % 2) == 0:
+#         print(str(i) + " Is an even number")
+#     else:
+#         print(str(i) + " Is an odd number")
+
+#loop, conditions, variables, 
+
+
+
+
+
---
+++ start.py
@@ -0,0 +1,29 @@
+#   Printing
+print("Hello world")
+
+#   Variables
+#   Integer addition
+a = 10
+b = 20
+c = 3
+print("Sum = ", a-b)
+
+#   Floatig point adiition
+q = 1.5
+p = 1.5
+print("Float sum = ", p+q)
+
+#   Boolean
+f = True
+print("Boolean ",f)
+
+#   Arthemetic operators
+# +, -, *, /, %
+m = a * b
+print("Product of a*b = ",m)
+
+#   Logical operators
+# or, and , ==, !=, >,<
+
+
+
