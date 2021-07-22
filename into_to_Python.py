5350f57af35118591260d48a1e78a820c8260e24
Joel S Joy <52008820+joelsjoyt@users.noreply.github.com>
2021-07-22 18:30:38 +0530

    Updated core files
    Added modified basic files

---
+++ into_to_Python.py
@@ -0,0 +1,16 @@
+"""
+This is Docstring
+"""
+
+#  This is Comment
+
+"""
+Data Types
+Int -  Intiger
+Str -  String
+Float - Floating point or decimal Values
+Bool  - Boolean Values aka True or False
+"""
+
+print("Put value to print to the user here") #Prints a string
+print(10) # prints a number
---
+++ variables_and_arthermtic_opr.py
@@ -0,0 +1,27 @@
+"""
+This is Day - 2 session
+on Variables 
+"""
+
+# Variable Declaration
+a = 17.5679
+b = 20
+
+# Arithemetic Operations
+sum = a + b      # Addition
+diff = a - b     # Difference
+prod  = a * b    # Product
+div = a / b      # Division    
+mod = a % b      # Remiander
+
+"""
+    Formatted string syntax
+    print(f"String to print here:{variable}")
+"""
+
+#Print using formatted string
+print(f"Sum of A  = {a}, B = {b}: {sum}")
+print(f"Diff of A  = {a}, B = {b}: {diff}")
+print(f"Prod of A  = {a}, B = {b}: {prod}")
+print(f"Div of A  = {a}, B = {b}: {div}")
+print(f"Mod of A  = {a}, B = {b}: {mod}")
